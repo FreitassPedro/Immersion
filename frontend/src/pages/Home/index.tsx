51a36bfd0fb03d0163dbc6691378a48c890de868
FreitasPedro <pedrospf2018@gmail.com>
2025-02-12 16:05:01 -0300

    :lipstick: refactor: grafico dashboard

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
 
 export const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <Link to={"/ciclo"}> Ciclo</Link>
         </div>
     )   
 }
