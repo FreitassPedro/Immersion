537be674cc50779ab82711f9a50058f2a121c7be
FreitasPedro <pedrospf2018@gmail.com>
2025-02-12 18:14:09 -0300

    feat: Ciclo Register

--- frontend/src/components/CicloRegister/index.tsx
+++ frontend/src/components/CicloRegister/index.tsx
@@ -1,22 +1,18 @@
+import { Box, TextField } from "@mui/material";
 
 export const CicloRegister = () => {
     return (
         <div>
-            <i>icon</i>
-            <h2>Registrar estudo</h2>
-            <span>Registre seu tempo de estudo</span>
+           <Box component="form">Registrar Ciclo
 
-            <label htmlFor="">
-                <span>Data</span>
-                <input type="date" />
-            </label>
-            <label htmlFor="#">
-                <span>Matéria</span>
-                <input type="text" />
-            </label>
-            <label htmlFor="">
-                
-            </label>
+            <TextField
+                id="materia"
+                name="materia"
+                label="Matéria"
+                placeholder="Insira a matéria"
+                required
+                />
+           </Box>
         </div>
     );
 };
