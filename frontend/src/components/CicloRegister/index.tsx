441b17276ff5e39c78e2c6b5f47b4b28a6126b82
FreitasPedro <pedrospf2018@gmail.com>
2025-02-13 15:11:32 -0300

    Estilizacao formulario e adição inputs

--- frontend/src/components/CicloRegister/index.tsx
+++ frontend/src/components/CicloRegister/index.tsx
@@ -1,12 +1,9 @@
 import './styles.css';
 
 const CicloRegister = () => {
-
     return (
         <div className='ciclo-register'>
-
-            <div>
-
+            <div className='form-container'>
                 <form>
                     <h1>Registrar</h1>
                     <span>Insira informações sobre</span>
@@ -14,79 +11,85 @@ const CicloRegister = () => {
                         <label>
                             <div className="input-group">
                                 <span>Data</span>
-                                <input type="date" />
+                                <input type="date" placeholder="Selecione a data" />
                             </div>
                         </label>
                         <label>
                             <div className="input-group">
                                 <span>Liquido</span>
-                                <input type="number" />
+                                <input type="number" placeholder="Digite o valor líquido" />
                             </div>
                         </label>
                         <label>
                             <div className="input-group">
                                 <span>Pausa</span>
-                                <input type="number" />
+                                <input type="number" placeholder="Digite o tempo de pausa" />
                             </div>
                         </label>
                     </div>
-                    <label>
-                        <div className="input-group">
-                            <span>Matéria</span>
-                            <input type="text" />
-                        </div>
-                    </label>
-                    <label>
-                        <div className="input-group">
-                            <span>Método de estudo</span>
-                            <select name="metodo" id="metodo">
-                                <option value="">Video</option>
-                                <option value="">Exercícios</option>
-                                <option value="">Leitura</option>
-                                <option value="">Flashcard</option>
-                                <option value="">Simulado</option>
-                            </select>
-                        </div>
-                    </label>
-                    <label>
-                        <div className="input-group">
-                            <span>Tags</span>
-                            <select name="tags" id="tags">
-                                <option value="">Revisar</option>
-                                <option value="">Falta atenção</option>
-                                <option value="">Finalizado</option>
-                                <option value="">Dúvidas</option>
-                                <option value="">Simulado</option>
-                            </select>
-                        </div>
-                    </label>
-
+                    <div className='form-group'>
+                        <label>
+                            <div className="input-group">
+                                <span>Matéria</span>
+                                <input type="text" placeholder="Digite a matéria" />
+                            </div>
+                        </label>
+                    </div>
+                    <div className='form-group'>
+                        <label>
+                            <div className="input-group">
+                                <span>Método de estudo</span>
+                                <select name="metodo" id="metodo">
+                                    <option value="" placeholder="Selecione o método">Selecione o método</option> {/* Placeholder no select */}
+                                    <option value="video">Vídeo</option>
+                                    <option value="exercicios">Exercícios</option>
+                                    <option value="leitura">Leitura</option>
+                                    <option value="flashcard">Flashcard</option>
+                                    <option value="simulado">Simulado</option>
+                                </select>
+                            </div>
+                        </label>
+                    </div>
+                    <div className='form-group'>
+                        <label>
+                            <div className="input-group">
+                                <span>Tags</span>
+                                <select name="tags" id="tags">
+                                    <option value="" placeholder="Selecione a tag">Selecione a tag</option> {/* Placeholder no select */}
+                                    <option value="revisar">Revisar</option>
+                                    <option value="falta-atencao">Falta atenção</option>
+                                    <option value="finalizado">Finalizado</option>
+                                    <option value="duvidas">Dúvidas</option>
+                                    <option value="simulado">Simulado</option>
+                                </select>
+                            </div>
+                        </label>
+                    </div>
                     <div className="form-group">
                         <label>
                             <div className="input-group">
                                 <span>Exercícios feitos</span>
-                                <input type="number" />
+                                <input type="number" placeholder="Digite o número de exercícios" />
                             </div>
                         </label>
                         <label>
                             <div className="input-group">
                                 <span>Acertos</span>
-                                <input type="number" />
+                                <input type="number" placeholder="Digite o número de acertos" />
                             </div>
                         </label>
                     </div>
-
                     <div className="form-group">
                         <label>
                             <div className="input-group">
                                 <span>Foco</span>
-                                <input type="range" min="1" max="5" step="0.5" />
+                                <input type="range" min="1" max="5" step="0.5" /> {/* Range não tem placeholder */}
                             </div>
                         </label>
                         <label>
                             <div className="input-group">
                                 <span>Produtividade</span>
-                                <input type="range" min="1" max="5" step="0.5" />
+                                <input type="range" min="1" max="5" step="0.5" /> {/* Range não tem placeholder */}
                             </div>
                         </label>
                     </div>
