e10697ad4f2d8fb963cf736b7bfd20944cec91d3
FreitasPedro <pedrospf2018@gmail.com>
2025-02-24 17:49:32 -0300

    refactor: Calculação das horas TimeInput

--- frontend/src/components/CicloRegister/TimeInput.tsx
+++ frontend/src/components/CicloRegister/TimeInput.tsx
@@ -1,31 +1,19 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./styles.module.css";
 
-const TimeInput = ({ initialTime }:
-    { initialTime: number }) => {
+interface TimeInputProps {
+    initialTime?: number; // o '?' indica que a propriedade é opcional
+}
+const TimeInput = ({ initialTime = 0 }: TimeInputProps) => {
+    const [hours, setHours] = useState(Math.floor(initialTime / 3600));
+    const [minutes, setMinutes] = useState(Math.floor((initialTime % 3600) / 60));
+    const [seconds, setSeconds] = useState(initialTime % 60);
 
-    const [hours, setHours] = useState<number>(0);
-    const [minutes, setMinutes] = useState<number>(0);
-    const [seconds, setSeconds] = useState<number>(0);
-
-    const [time, setTime] = useState<string>();
-
-    useEffect(() => {
-        if (initialTime > 0) {
-            const h = Math.floor(initialTime / 3600);
-            const m = Math.floor((initialTime % 3600) / 60);
-            const s = initialTime % 60;
-
-            setHours(h);
-            setMinutes(m);
-            setSeconds(s);
-        }
-        setTime(`${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`);
-    }, [hours, minutes, seconds]);
+    const formatTime = () =>
+        `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 
     return (
-        <div className={styles.timeinput} style={{}}>
-            {/* Selecionar Horas */}
+        <div className={styles.timeinput} >
             <select value={hours} onChange={(e) => setHours(Number(e.target.value))}>
                 {Array.from({ length: 24 }, (_, i) => (
                     <option key={i} value={i}>
@@ -53,7 +41,7 @@ const TimeInput = ({ initialTime }:
                     </option>
                 ))}
             </select>
-            <input type="hidden" name={"horasLiquidas"} value={time} />
+            <input type="hidden" name={"horasLiquidas"} value={formatTime()} />
         </div>
     );
 };
