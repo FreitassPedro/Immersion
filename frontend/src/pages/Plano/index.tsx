797c288625849ab9d6ac271d351c4e03534e76f4
FreitasPedro <pedrospf2018@gmail.com>
2025-02-25 17:16:09 -0300

    css: Plano

--- frontend/src/pages/Plano/index.tsx
+++ frontend/src/pages/Plano/index.tsx
@@ -10,7 +10,13 @@ export const Plano = () => {
 
                 <div className={styles["cards-group"]}>
                     <div className={styles["subject-card"]}>
-                        <div className={styles["subject-header"]}>Física</div>
+                    <div className={styles["subject-header"]}>
+                            <h2>Física</h2>
+                            <div className={styles["subject-header-hour"]}>
+                                <i className="fa-light fa-clock" />
+                                <span>02:30</span>
+                            </div>
+                        </div>
                         <div className={styles["sessions-container"]}>
                             <div className={styles["session-card"]}>1h</div>
                             <div className={styles["session-card"]}>1h 20min</div>
@@ -19,18 +25,51 @@ export const Plano = () => {
                     </div>
 
                     <div className={styles["subject-card"]}>
-                        <div className={styles["subject-header"]}>Matemática</div>
+                    <div className={styles["subject-header"]}>
+                            <h2>Matemática</h2>
+                            <div className={styles["subject-header-hour"]}>
+                                <i className="fa-light fa-clock" />
+                                <span>02:30</span>
+                            </div>
+                        </div>
                         <div className={styles["sessions-container"]}>
                             <div className={styles["session-card"]}>1h</div>
                             <div className={styles["session-card"]}>1h 20min</div>
                             <div className={styles["session-card"]}>1h 20min</div>
                             <div className={styles["session-card"]}>1h 20min</div>
-
                             <div className={styles["session-card"]}>1h 20min</div>
                             <div className={styles["session-card"]}>1h 20min</div>
-
                             <div className={styles["session-card"]}>1h 20min</div>
+                        </div>
+                    </div>
 
+                    <div className={styles["subject-card"]}>
+                        <div className={styles["subject-header"]}>
+                            <h2>Quimica</h2>
+                            <div className={styles["subject-header-hour"]}>
+                                <i className="fa-light fa-clock" />
+                                <span>02:30</span>
+                            </div>
+                        </div>
+                        <div className={styles["sessions-container"]}>
+                            <div className={styles["session-card"]}>1h</div>
+                            <div className={styles["session-card"]}>1h 20min</div>
+                            <div className={styles["session-card"]}>1h 20min</div>
+                        </div>
+                    </div>
+
+                    <div className={styles["subject-card"]}>
+                        <div className={styles["subject-header"]}>
+                            <h2>Biologia</h2>
+                            <div className={styles["subject-header-hour"]}>
+                                <i className="fa-light fa-clock" />
+                                <span>02:30</span>
+                            </div>
+                        </div>
+                        <div className={styles["sessions-container"]}>
+                            <div className={styles["session-card"]}>1h</div>
+                            <div className={styles["session-card"]}>1h 20min</div>
+                            <div className={styles["session-card"]}>1h 20min</div>
                         </div>
                     </div>
                 </div>
