28d37c3ed22da9eba48308c8c678579411a8b5a1
FreitasPedro <pedrospf2018@gmail.com>
2025-02-28 10:26:44 -0300

    feat: Collapse Expand sidebar

--- frontend/src/components/SideBar/index.tsx
+++ frontend/src/components/SideBar/index.tsx
@@ -1,65 +1,80 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
 
+export const SideBar: React.FC = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-export const SideBar = () => {
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
 
-    return (
-        <nav className={styles.sidebar}>
+  return (
+    <nav className={`${styles.sidebar} ${isCollapsed ? styles.collapsed : ''}`}>
+      <div className={styles.sidebarHeader}>
+        <h1 className={styles.sidebarTitle}>Immersion</h1>
+        <button 
+          className={styles.toggleButton}
+          onClick={toggleSidebar}
+          aria-label={isCollapsed ? "Expandir sidebar" : "Recolher sidebar"}
+        >
+          <span>≡</span>
+        </button>
+      </div>
+      
+      <div className={styles.divider}></div>
+      
+      <ul className={styles.menuList}>
+        <li className={styles.menuItem}>
+          <a href="#" className={styles.menuLink}>
+            <i className="fa-light fa-house"></i>
+            <span className={styles.menuText}>Home</span>
+          </a>
+        </li>
 
-            <div className={styles.navHeader} >
-                <h1>Immersion</h1>
-                <div className={styles.btnTogle}>
-                    <i className="fa-duotone fa-light fa-bars"></i>
-                </div>
-            </div >
-            <hr />
-            <div className={styles.navContent}>
-                <li className={styles.navItem}>
-                    <a href="#">
-                        < i className="fa-light fa-house"></i>
-                        < span > Home</span >
-                    </a >
-                </li >
+        <li className={styles.menuItem}>
+          <Link to="/ciclo" className={styles.menuLink}>
+            <i className="fa-light fa-clipboard"></i>
+            <span className={styles.menuText}>Ciclo</span>
+          </Link>
+        </li>
+        
+        <li className={styles.menuItem}>
+          <Link to="/registro" className={styles.menuLink}>
+            <i className="fa-light fa-clock-rotate-left"></i>
+            <span className={styles.menuText}>Registros</span>
+          </Link>
+        </li>
 
-                <li className={styles.navItem}>
-                    < Link to="/ciclo" >
-                        <i className="fa-light fa-clipboard"></i>
-                        < span > Ciclo</span >
-                    </Link >
-                </li >
-                <li className={styles.navItem}>
-                    < Link to="/registro" >
-                        <i className="styles.fa-light fa-clock-rotate-left" />
-                        < span > Registros</span >
-                    </Link >
-                </li >
+        <li className={styles.menuItem}>
+          <a href="#" className={styles.menuLink}>
+            <i className="fa-light fa-chart-line"></i>
+            <span className={styles.menuText}>Desempenho</span>
+          </a>
+        </li>
 
-                <li className={styles.navItem}>
-                    <a href="#" >
-                        <i className="fa-light fa-chart-line"></i>
-                        <span> Desempenho</span>
-                    </a>
-                </li>
+        <li className={styles.menuItem}>
+          <a href="#" className={styles.menuLink}>
+            <i className="fa-light fa-gear"></i>
+            <span className={styles.menuText}>Configurações</span>
+          </a>
+        </li>
+      </ul>
 
-                <li className={styles.navItem}>
-                    <a href="#" >
-                        <i className="fa-light fa-gear"></i>
-                        <span> Configurações</span >
-                    </a>
-                </li>
-            </div>
-
-            <div className={styles.navFooter}>
-                < div className={styles.navFooterHead}>
-                    <div className={styles.navFooterAvatar}>
-                        <img src="https://cdn-icons-png.flaticon.com/512/3242/3242257.png" alt="" />
-                    </div >
-                    <div className={styles.navFooterUser}>
-                        <strong> 'Usuário nick'</strong>
-                    </div>
-                </div >
-            </div >
-        </nav >
-    );
+      <div className={styles.sidebarFooter}>
+        <div className={styles.userProfile}>
+          <div className={styles.avatarWrapper}>
+            <img 
+              src="https://cdn-icons-png.flaticon.com/512/3242/3242257.png" 
+              alt="Avatar do usuário" 
+              className={styles.avatarImage}
+            />
+          </div>
+          <div className={styles.userInfo}>
+            <span className={styles.userName}>Usuário</span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
